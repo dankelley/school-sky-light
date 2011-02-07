85ac32e0ecdc8fef5d4ec5b29c03d59d58912908
Dan Kelley <kelley.dan@gmail.com>
2011-02-06 23:08:27 -0400

    more diagnostics

--- sun/sunrise.R
+++ sun/sunrise.R
@@ -2,7 +2,7 @@ library(oce)
 use.refraction <- TRUE
 D <- 2.0
 elevation.misfit <- function(e) {
-    mean(ifelse(e > 0, abs(e), D + abs(e)))
+    mean(ifelse(e > 0, abs(e), 10*abs(e)))
 }
 angle.misfit.1 <- function(x) { # lat
     elevation <- sun.angle(t0, x[1], lon, use.refraction)$elevation
@@ -33,18 +33,20 @@ tz <- 4 * 3600
 sunrise <- as.POSIXct(paste(d$V1, d$V2), tz="UTC") + tz
 sunset <- as.POSIXct(paste(d$V1, d$V3), tz="UTC") + tz + 12 * 3600
 # http://en.wikipedia.org/wiki/Angular_diameter
+tex <- 0.75 
 if (FALSE) {
     sun.time <- 31/60 / 360 * 24 * 3600
     sunrise <- sunrise + sun.time
     sunset <- sunset - sun.time
 }
 mod <- seq(-10, 10, 0.1)
-par(mfrow=c(2,1), mar=c(3,3,1,1), mgp=c(3/2,2/3,0))
+par(mfrow=c(4,1), mar=c(3,3,1,1), mgp=c(3/2,2/3,0))
 for (s in 1:length(d$V1)) {
     cat("s=",s,"\n")
+    ## Sunrise
     elevation <- NULL
     for (m in mod)
-        elevation <- c(elevation, sun.angle(sunset[s], lat+m, lon, use.refraction)$elevation)
+        elevation <- c(elevation, sun.angle(sunrise[s], lat+m, lon, use.refraction)$elevation)
     plot(lat+mod, elevation, xlab="Latitude", ylab="Elevation", type='l')
     ## optimized fits
     t0 <- sunrise[s]
@@ -52,14 +54,40 @@ for (s in 1:length(d$V1)) {
     lonfit <- optim(c(0),angle.misfit.2)$par
     abline(h=0)
     abline(v=lat, col='blue')
-    mtext(format(sunset[s]), adj=1, line=-1)
-    mtext("sunset", adj=1, line=-2)
+    mtext(paste("Sunrise:", format(sunrise[s])), adj=1, line=-1, cex=tex)
     mtext(paste("actual: ", format(lat, digits=5), "N  ", format(lon,digits=5), "E", sep=""),
-          adj=1, line=-3, col='blue')
+          adj=1, line=-2, col='blue', cex=tex)
     mtext(paste("fit:    ", format(latfit, digits=5), "N  ", format(lonfit,digits=5), "E", sep=""),
-          adj=1, line=-4, col='red')
+          adj=1, line=-3, col='red', cex=tex)
     error <- geod.dist(lat, lon, latfit, lonfit)
-    mtext(sprintf("Misfit: %.1f km", error), adj=1, line=-5)
+    mtext(sprintf("Misfit: %.1f km", error), adj=1, line=-4, cex=tex)
+    abline(v=latfit, col='red')
+    elevation <- NULL
+    for (m in mod)
+        elevation <- c(elevation, sun.angle(sunrise[s], lat, lon+m, use.refraction)$elevation)
+    plot(lon+mod, elevation, xlab="Longitude", ylab="Elevation", type='l')
+    abline(h=0)
+    abline(v=lon, col='blue')
+    abline(v=lonfit, col='red')
+
+    ## Sunset
+    elevation <- NULL
+    for (m in mod)
+        elevation <- c(elevation, sun.angle(sunset[s], lat+m, lon, use.refraction)$elevation)
+    plot(lat+mod, elevation, xlab="Latitude", ylab="Elevation", type='l')
+    ## optimized fits
+    t0 <- sunset[s]
+    latfit <- optim(c(0),angle.misfit.1)$par
+    lonfit <- optim(c(0),angle.misfit.2)$par
+    abline(h=0)
+    abline(v=lat, col='blue')
+    mtext(paste("Sunset:", format(sunset[s])), adj=1, line=-1, cex=tex)
+    mtext(paste("actual: ", format(lat, digits=5), "N  ", format(lon,digits=5), "E", sep=""),
+          adj=1, line=-2, col='blue', cex=tex)
+    mtext(paste("fit:    ", format(latfit, digits=5), "N  ", format(lonfit,digits=5), "E", sep=""),
+          adj=1, line=-3, col='red', cex=tex)
+    error <- geod.dist(lat, lon, latfit, lonfit)
+    mtext(sprintf("Misfit: %.1f km", error), adj=1, line=-4, cex=tex)
     abline(v=latfit, col='red')
     elevation <- NULL
     for (m in mod)
