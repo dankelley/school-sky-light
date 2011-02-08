d2eceb61f3afbff788abec297df22c5cd2a4ef0b
Dan Kelley <kelley.dan@gmail.com>
2011-02-08 14:30:04 -0400

    sunrise

--- sun/sunrise.R
+++ sun/sunrise.R
@@ -1,7 +1,7 @@
 ##http://www.timeanddate.com/worldclock/astronomy.html?n=286&month=2&year=2011&obj=sun&afl=-11&day=1
 library(oce)
 use.refraction <- TRUE
-angle.misfit.1 <- function(x) { # lat
+above.horizon <- function(x) { # given longitude
     elevation <- sun.angle(t0, x[1], lon, use.refraction)$elevation
     mean(elevation^2)^0.5
 }
@@ -18,20 +18,20 @@ print(data.frame(sunrise,sunset))
 data(coastline.world)
 
 pdf("map-with-lines.pdf", width=8, height=10.5, pointsize=12)
+n <- 100
+vlon <- seq(-180, 180, length.out=n)
+vlat <- vector(length=n)
 t0 <- sunrise[1]
-vlon <- vlat <- NULL 
-for (lon in seq(-180, 180, 2)) {
-    vlon<-c(vlon, lon)
-    vlat<-c(vlat, optim(c(0),angle.misfit.1)$par)
+for (i in 1:length(vlon)) {
+    vlat[i] <- optimise(above.horizon, interval=c(-90,90))$minimum
 }
 plot(coastline.world, center=c(hlat, hlon), span=1500)
 abline(h=hlat, v=hlon, col='gray')
 lines(vlon, vlat, lwd=2)
 t0 <- sunset[1]
-vlon <- vlat <- NULL 
-for (lon in seq(-180, 180, 2)) {
-    vlon<-c(vlon, lon)
-    vlat<-c(vlat, optim(c(0),angle.misfit.1)$par)
+for (i in 1:length(vlon)) {
+    ##vlat[i] <- optim(0, above.horizon)$par
+    vlat[i] <- optimise(elevation, interval=c(-90,90))$minimum
 }
 lines(vlon, vlat, lwd=2, lty="dashed")
 legend("bottomright", lwd=c(2,2), lty=c("solid", "dashed"),legend=c("Sunrise", "Sunset"))
