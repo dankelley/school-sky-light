d8165673679db4ba126ec5b61cb189f9daaf04c2
Dan Kelley <kelley.dan@gmail.com>
2011-03-03 16:06:20 -0400

    reword concept page

--- web/concept.php
+++ web/concept.php
@@ -1,40 +1,35 @@
 <!-- vim:set spell filetype=html: -->
-<p> The sky darkens quickly before a sudden downpour, and slowly as a front
-comes in over the course of a few hours.  This is why, even with modern
-forecasts, people rely on sky brightness as a rough predictor of local weather
-changes over short time intervals.  This works locally, but it could be made to
-work over larger distances.  If a storm rolls northeast up the coast of Nova
-Scotia, skies will darken in Yarmouth a few hours before bad weather gets to
-Halifax.  The same goes for other directions of storm travel, and so a network
-of observers could create a crude "weather forecast" simply by observing skies
-and making phone calls.  </p>
+<p>The sky darkens in the minutes before a summer-afternoon downpour.  The
+slower arrival of fronts is signalled by a slower darkening.  Such facts
+explain why, despite modern weather forecasts, people still use sky brightness
+as a predictor of local weather.  All it takes is a little communication to
+leverage this into forecasts of regional weather.  For example, a storm moving
+up the coast of Nova Scotia will darken skies in Yarmouth a few hours before
+bad weather gets to Halifax, and so a few well-placed phone calls could improve
+the range of forecasts for Halifax.</p>
 
-<p> But most people can't watch the sky all day, and are not willing to make
-dozens of phone calls every hour.  Luckily, it is easy to automate the whole
-process.  This is where SkyNet comes in.  The idea is to place sky brightness
-sensors in the schools.  This has the advantage of providing good coverage
-across populated regions.  Also, schools have computers that can be used to log
-the observations and distribute them.  And, finally and most importantly, the
-process provides diverse learning experiences for students.  </p>
+<p>But not everyone can spend the day watching the sky and chatting on the
+phone.  The solution is to set up a network of automated light sensors.  This
+could be done in a professional way, at professional costs.  The SkyNet program
+proposes another solution: do it all for essentially no costs.  Put the sensors
+in the schools.  This will yield good spatial coverage, because all populated
+areas have schools.  It will also cost almost nothing, because the students
+will be doing the work of constructing, installing, and maintaining the light
+sensors.  Far from an exercise in modern-day child labour, SkyNet is actually a
+thinly-disguised learning experience.  The experience could be very broad, indeed.
+Science-minded students would benefit from observing the atmosphere, and
+thinking about the Physics involved.  Engineering-minded students would benefit
+from setting up and calibrating the equipment.  There are obvious opportunities for
+students interested in statistics and computing.  Business-minded students
+could organize the work, mind the finances, deal with local sponsors, etc.
+Good writers will be needed to communicate with the public.  Students
+interested in history will find that SkyNet is using principles that tie
+directly to generations-old methods of celestial navigation.  The list goes on;
+the point is that SkyNet could offer much, to many students.</p>
 
-<p>The devices themselves (described elsewhere on this website) would be
-constructed by the students.  And the students would also calibrate the
-sensors, invent and perform statistical analyses, pour the data into web
-streams, and so forth.  In doing this work (which they would likely regard as
-play), they would learn about much more than meteorology.  Technically-minded students
-would enjoy honing skills in engineering, electronics, statistics, and
-computing.  Business-minded students could participate by organizing team-work,
-working with sponsors, or commercializing their results.  Students interested
-in writing could do public outreach.  Historians won't be left out, because
-studying the sky was the basis of navigation until just a few decades ago.  The
-list goes on; the point is that SkyNet could offer much, to many students.</p>
-
-<p>There is no reason for the project to end with the measurement of light.
-Students could also design new sensors, to measure such things as barometric
-pressure, humidity, and wind speed.  Many things that were complicated to
-measure a generation ago are now within easy reach.  We are living in an era of
-do-it-yourself computing and microcontrol, a particularly relevant component of
-which is the growing interest in robotics in schools.  </p>
+<p>There project need not be limited to the measurement of light.  With modern
+micro-controllers and sensors, students could measure many aspects of weather
+that were once the domain of the professionals.</p>
 
 <p>It seems likely that people would start to consult SkyNet for informal
 short-term local weather forecasts.  Can anyone doubt the excitement, and
