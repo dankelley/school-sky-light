b2a572a9fcae8bfb47ffb674f5c44616c9eeda46
Dan Kelley <kelley.dan@gmail.com>
2011-03-05 11:59:27 -0400

    working on solar nav

--- web/code/solar_navigation_2.R
+++ web/code/solar_navigation_2.R
@@ -1,100 +1,68 @@
 library(oce)
 if (!interactive())
-png("solar_navigation_timeseries.png", width=800, height=300, pointsize=12)
-
+    png("solar_navigation_timeseries.png", width=800, height=300, pointsize=12)
 mismatch <- function(latlon) 
 {
-sun.angle(rise, latlon[1], latlon[2])$elevation^2 + sun.angle(set, latlon[1], latlon[2])$elevation^2
+    sun.angle(rise, latlon[1], latlon[2])$elevation^2 +
+    sun.angle(set, latlon[1], latlon[2])$elevation^2
 }
 
 hfx.sun.angle <- function(t) sun.angle(t, lat=44+39/60, lon=-(63+36/60))$elevation
 d <- read.table("http://emit.phys.ocean.dal.ca/~kelley/skynet/skynet-01.dat", header=FALSE)
-#d <- read.table("../skynet-01.dat", header=FALSE)
+##d <- read.table("../skynet-01.dat", header=FALSE)
 time <- as.POSIXct(paste(d$V1, d$V2), tz="UTC") + 4 * 3600
 light <- (100*(1023-d$V3)/1023)
-## FIXME: The nighttime level changed on March 1 (for reasons unknown), so the 
-## FIXME: best 'dark' in the beginning is 1/2, but later it needs to be 3 or so.
-## FIXME: We need a runmin() function.  Would that be hard to implement in oce?
-dark <- 2
-light.smoothed <- supsmu(time, light, span=30/length(light))$y
-light.smoothed <- predict(smooth.spline(as.numeric(time)-as.numeric(time[1]), light, df=length(light)/10))$y
-light.smoothed <- runmed(light, k=5)
-sunrise.inferred <- time[which(diff(light.smoothed > dark) == 1)]
-print(sunrise.inferred)
-sunset.inferred <- time[which(diff(light.smoothed > dark) == -1)]
-print(sunset.inferred)
-if (sunrise.inferred[1] > sunset.inferred[1])
-sunrise <- sunrise[-1]
-ndays <- min(length(sunrise.inferred), length(sunset.inferred))
 oce.plot.ts(time, light, ylab="Light intensity (percent)", ylim=c(0, 100))
-
+light.smoothed <- kernapply(light, kernel("daniell", 3), circular=TRUE)
+dim.light <- ifelse(light.smoothed < 5, light, runif(length(light), 0, 5))
+light.floor <- runmed(dim.light, k=25*60+1)
+dn <- smooth(as.numeric((light.smoothed - light.floor) > 1))
+lines(time, light.floor, col='blue', lty='dashed')
+rises <- time[which(diff(dn) > 0)]
+sets <- time[which(diff(dn) < 0)]
+## Trim false starts
+if (rises[1] > sets[1])
+    sets <- sets[-1]
+## Only consider 'rises' and 'sets' in pairs
+nrises <- length(rises)
+if (length(sets) < nrises)
+    rises <- rises[1:length(sets)]
+if (length(sets) > nrises)
+    sets <- sets[1:nrises]
+nrises <- length(rises)
+rug(rises, col='red', lwd=4)
+rug(sets, col='blue', lwd=4)
 ## Distances
 lat.hfx <- 44.65
 lon.hfx <- (-63.55274)
 lats <- NULL
 lons <- NULL
-for (day in 1:ndays) {
-    rise <- sunrise.inferred[day] + 0*4*3600
-    set <- sunset.inferred[day] + 0*4*3600
+for (i in 1:nrises) {
+    rise <- rises[i] + 0*4*3600
+    set <- sets[i] + 0*4*3600
     result <- optim(c(1,1), mismatch)
     dist <- geod.dist(result$par[1], result$par[2], lat.hfx, lon.hfx)
     lats <- c(lats, result$par[1])
     lons <- c(lons, result$par[2])
-    print(dist)
+    cat(i, nrises, format(rise), format(set), dist, "\n")
 }
 
-tr <- range(time)
-days <- seq(trunc(tr[1], "days"), trunc(tr[2]+86400), "days")
-days <- days[-1]                       # skip first day, which may be partial
-days <- days[-length(days)]            # skip last day, which may be partial
-ndays <- length(days)
-print(days)
-sunrise.inferred <- sunset.inferred <- NULL
-lats <- lons <- NULL
-for (iday in 1:(ndays-1)) {
-    look <- days[iday] <= time & time <= days[iday+1]
-    dark <- min(light.smoothed[look])
-    dark2 <- median(light.smoothed[look])
-    if (iday == 2) {
-        dan.dark2<<-dark2
-        dan.light<<-light[look]
-        dan.light.smoothed<<-light.smoothed[look]
-        dan.time<<-time[look]
-    }
-    diff <- diff(light.smoothed[look] > dark2)
-    rise <- time[look][which(diff == 1)][1]
-    sunrise.inferred <- c(sunrise.inferred, rise)
-    set <- time[look][which(diff == -1)][1]
-    sunset.inferred <- c(sunset.inferred, set)
-    result <- optim(c(1,1), mismatch)
-    lats <- c(lats, result$par[1])
-    lons <- c(lons, result$par[2])
-    points(result$par[1], result$par[2], col='red')
-    dist <- geod.dist(result$par[1], result$par[2], lat.hfx, lon.hfx)
-    cat("iday=", iday,
-        "sunrise at", format(number.as.POSIXct(sunrise.inferred[iday])),
-        "sunset at", format(number.as.POSIXct(sunset.inferred[iday])),
-        "(dark=", dark, " dark2=", dark2, ") dist=", dist, "\n")
-                                        #    result <- optim(c(1,1), mismatch)
-                                        #    sunrise <- oce.bisect(hfx.sun.angle, day, day + 12 * 3600)
-                                        #    sunset <- oce.bisect(hfx.sun.angle, day + 12*3600, day + 24 * 3600)
-}
-sunrise.inferred <- number.as.POSIXct(sunrise.inferred)
-sunset.inferred <- number.as.POSIXct(sunset.inferred)
-abline(v=sunrise.inferred, col='red')
-abline(v=sunset.inferred, col='blue')
-legend("topleft", lwd=c(1, 1, 1, 1),
-       col=c("black", "red", "red", "blue", "blue"),
-       lty=c("solid", "dotted", "solid", "dotted", "solid"),
-       legend=c("Observed", "Sunrise [inferred]", "Sunrise [theoretical]", "Sunset [inferred]", "Sunset [theoretical]"),
+legend("topleft", lwd=c(1, 1, 1, 1), 
+       col=c("black", "blue", "red", "blue"),
+       lty=c("solid", "dashed", "solid", "solid"),
+       legend=c("Observed", "Dark level", "Sunrise", "Sunset"),
        cex=3/4, bg="white")
 dev.off()
 
 ## map
 if (!interactive())
-    png("solar_navigation_map.png", width=800, height=800, pointsize=12)
+    png("solar_navigation_map.png", width=800, height=600, pointsize=12)
 par(mfrow=c(1,1))
 data(coastline.world)
-plot(coastline.world, center=c(lat.hfx, lon.hfx), span=8000)
-points(lons, lats, pch=21, cex=2, bg='white', lwd=5)
+plot(coastline.world, center=c(lat.hfx, lon.hfx), span=3000)
+points(lons, lats, pch=21, cex=2, bg='white', col='blue', lwd=5)
+set <- mean(sets)
+rise <- mean(rises)
+overall <- optim(c(1,1), mismatch)$par
+points(overall[2], overall[1], cex=4, bg="white", col='red', lwd=5)
 
--- web/results.php
+++ web/results.php
@@ -3,54 +3,54 @@
 parse_str($_SERVER['QUERY_STRING']);
 
 if ("$subtab" == "") {
-echo '<p>
-<a href="?tab=results&amp;subtab=weather">Weather</a>
-|
-<a href="?tab=results&amp;subtab=solar_navigation">Solar Navigation</a>
-</p>';
-}
-
-if ("$subtab" == "weather") {
-echo '<p>
-<p>
+    echo '<p>
+        <a href="?tab=results&amp;subtab=weather">Weather</a>
+        |
+        <a href="?tab=results&amp;subtab=solar_navigation">Solar Navigation</a>
+        </p>';
+} elseif ("$subtab" == "weather") {
+        echo '<p>
 Weather
 | 
 <a href="?tab=results&amp;subtab=solar_navigation">Solar Navigation</a>
 </p>';
-echo '
-<h1>Weather</h1>
-<p>
-<img src="code/plot.png" alt="plot"/>
-</p>
 
-<p>
-The graph indicates temporal variation of light intensity in an office in 
-Halifax, Nova Scotia, updated at 10-minute intervals based on 1-minute averages 
-of data measured at 10-second intervals.  Readings of about 80% result from the 
-use of overhead lights in the office.
-</p>
-';
+echo '
+    <h1>Weather</h1>
+
+    <p><img src="code/plot.png" alt="plot"/></p>
+
+    <p>The graph shown above indicates temporal variation of light intensity in an
+    office in Halifax, Nova Scotia, updated at 10-minute intervals based on
+    1-minute averages of data measured at 10-second intervals.  The sensor is
+    placed on a desk, pointing upwards, and shaded from direct sunlight.  The sharp
+    transitions to light levels of approximately 80% result when the lights are
+    turned on in the office.</p>
+
+    ';
+
+} elseif ("$subtab" == "solar_navigation") {
+    echo '
+        <p>
+        <a href="?tab=results&amp;subtab=weather">Weather</a>
+        | 
+        Solar Navigation
+        </p>';
+
+    echo '
+
+        <h1>Solar Navigation</h1>
+
+        <p> <img src="code/sunrise_sunset.png" alt="sunrise_sunset.png"/> </p>
+
+        <p>The graph shown above is the result of early tests with detection of sunrise
+        and sunset times.  Students could use these times to infer the location of the
+        observation, putting them in touch with something that their seafaring
+        ancestors would have understood easily.</p>
+
+        '
+        ;
 }
 
-if ("$subtab" == "solar_navigation") {
-echo '<p>
-<p>
-<a href="?tab=results&amp;subtab=weather">Weather</a>
-| 
-Solar Navigation
-</p>';
-echo '
-<h1>Solar Navigation</h1>
-<p>
-The graph below shows some early tests with detection of sunrise and sunset 
-times.  Students could use these times to infer the location of the 
-observation, putting them in touch with something that their seafaring 
-ancestors would have understood easily.
-</p>
-
-<p>
-<img src="code/sunrise_sunset.png" alt="sunrise_sunset.png"/>
-</p>';
-}
 ?>
 
