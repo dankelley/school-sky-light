48cb85b04ea8b22d65059df6cd04e1e89b336924
Dan Kelley <kelley.dan@gmail.com>
2011-03-14 12:14:34 -0300

    skynet2 now offline

--- web/methods.php
+++ web/methods.php
@@ -10,11 +10,13 @@ href="code/slave_logger.pde">slave_logger.pde</a> and the logging computer
 interfaces to this with a C program called <a href="code/msl.c">msl.c</a>.
 The logging computer also creates the graphs shown on this website at regular intervals, using the unix "cron" system.</p>
 
-<p> <b>Data.</b> There are two sensors working at the moment, <a
-    href="skynet-01.dat">skynet-01.dat</a> and <a
-    href="skynet-02.dat">skynet-02.dat</a>.  An image of them, and their connected micro-controllers, is below.
-Note that one is more shaded than another, at the particular moment of the photo; the shading changes through the hours 
-of the day.
+<p> <b>Data.</b> There is one sensor working at the moment,
+called <href="skynet-01.dat">skynet-01.dat</a>.
+An image of this, along with a pair that was sitting near it for a while, is
+given
+below.
+Note that one is more shaded than another, at the particular moment of the 
+photo; the shading changes through the hours of the day.
 <img src="images/sensors_phase1.jpg" alt="sensors_phase1"/>
 </p>
 
