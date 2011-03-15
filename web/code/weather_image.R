1d23233213dad3e680c0504ec0e74a0e72eb420f
Dan Kelley <kelley.dan@gmail.com>
2011-03-15 11:05:07 -0300

    avoid mixup on e.g. daylight saving time

--- web/code/weather_image.R
+++ web/code/weather_image.R
@@ -1,12 +1,14 @@
 library(oce)
+deltat <- 60 # seconds between sample (nominal)
 d <- read.table('../skynet-01.dat', header=FALSE)
 time <- as.POSIXct(paste(d$V1, d$V2))
 light  <- 100 * (1023 - d$V3) / 1023
-time.g <- seq(trunc(time[1], "day"), 86400 + trunc(time[length(time)], "day"), by=60)
+time.g <- seq(trunc(time[1], "day"), 86400 + trunc(time[length(time)], "day"), by=deltat)
 light.g <- approx(time, light, time.g, rule=1)$y
-days <- floor(length(light.g)/24/60)
-data.per.day <- 24 * 60
-light.m <- matrix(light.g[1:(days*data.per.day)], ncol=86400/60, byrow=TRUE)
+time.range <- range(time.g)
+days <- floor(0.5 + as.numeric(difftime(time.range[2], time.range[1], "days"))) # round for e.g. daylight-time
+data.per.day <- 86400 / deltat
+light.m <- matrix(light.g[1:(days*data.per.day)], ncol=data.per.day, byrow=TRUE)
 png("weather_image.png", width=700, height=250, pointsize=13)
 time.axis <- as.POSIXct(seq.POSIXt(min(time.g), min(time.g)+(days-1)*86400, by="day"))
 imagep(time.axis, (1:data.per.day)/60, light.m, xlab="", ylab="Hour",
