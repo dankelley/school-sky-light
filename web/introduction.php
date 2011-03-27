d69caf6b6d748226accde9c45abd25371a7adb7c
Dan Kelley <kelley.dan@gmail.com>
2011-03-27 09:45:46 -0300

    reword intro

--- web/introduction.php
+++ web/introduction.php
@@ -1,34 +1,34 @@
 <!-- vim:set spell filetype=php: -->
 
-<p>
-The sky darkens in the minutes before an afternoon downpour.  Patient observers 
-will notice that the sky darkens in the hours before a front arrives.  These 
-are predictions of local weather that might be valid across a city.  Since weather moves in 
-patterns, one might wonder whether a network of sky observers might be able to
-make predictions that would be useful over larger regions, such as provinces.</p>
+<p> Most of us have noticed that the sky darkens a short while before an 
+afternoon downpour.  And keen weather observers have the patience to monitor a 
+darkening over several hours before fronts arrive.  These are examples of 
+<i>local</i> weather prediction based on variations in sky brightness.   
+<i>Regional</i>  predictions could be made, if observations could be tied together.
+For example, a storm running up the coast of Nova Scotia might darken Lunenburg 
+skies a few hours before anything could be observed in Halifax.</p>
 
-<p>But not everyone can spend the day watching the sky and chatting on the
-phone.  The solution is to set up a network of automated light sensors.  This
-could be done in a professional way, at professional costs.  The SkyNet program
-proposes another solution: put the sensors in schools.  All populated areas
-have schools, so good spatial coverage is ensured.  And, since schools already
-have computers, data recording will not require new infrastructure.  As for the
-sensors, students are capable of building them, calibrating them, and
-interfacing them to computers.</p>
+<p>The question is how to set up a network with enough stations to be of use.  
+The Skynet program proposes an answer: put sensors in schools.  This will give 
+excellent spatial coverage, because all populated areas have schools.  And, 
+since schools already have computers, data recording will not require new 
+infrastructure.  As for the sensors, students are capable of building them, 
+calibrating them, and interfacing them to computers.</p>
 
-<p>This seems to be an exercise in child labour.  Far from it.  Actually,
-SkyNet is a thinly-disguised learning experience.  Science-minded students
-would benefit from observing the atmosphere, and thinking about the general
-Physics involved, including convection, waves, and fronts.  Students with
-interests in engineering and computing would gain much from setting up and
-calibrating the equipment, and linking sensors to computers.  There are obvious
-opportunities for students interested in statistics.  Management-minded students
-could organize the work teams, while business-minded students could take care
-of the finances and deal with local sponsors.  Writers will be needed to
-explain the work, and public speakers could communicate with the media and the
-public.  SkyNet will offer insights on history, because observing the sky was
-once a serious thing, for navigation, agriculture, and so forth.  The list goes
-on.  Simply stated, SkyNet could offer much, to many students.</p>
+<p>This seems to be an exercise in child labour.  Far from it.  Actually, 
+SkyNet is a thinly-disguised learning experience.  And it is a broad learning 
+experience, indeed. Science-minded students would benefit from observing the 
+atmosphere, and thinking about the general Physics involved, including 
+convection, waves, and fronts.  Students with interests in engineering and 
+computing would gain much from setting up and calibrating the equipment, and 
+linking sensors to computers.  There are obvious opportunities for students 
+interested in statistics.  Management-minded students could organize the work 
+teams, while business-minded students could take care of the finances and deal 
+with local sponsors.  Writers will be needed to explain the work, and public 
+speakers could communicate with the media and the public.  SkyNet will offer 
+insights on history, because observing the sky was once a serious thing, for 
+navigation, agriculture, and so forth.  The list goes on.  Simply stated, 
+SkyNet could offer much, to many students.</p>
 
 <p>There project need not be limited to the measurement of light.  With modern
 micro-controllers and sensors, students could measure many aspects of weather
