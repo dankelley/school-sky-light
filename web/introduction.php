6c081fb58fa59a3d4c337aec20686825ec754c8b
Dan Kelley <kelley.dan@gmail.com>
2011-04-09 12:20:07 -0300

    rename skyview

--- web/introduction.php
+++ web/introduction.php
@@ -1,41 +1,39 @@
 <!-- vim:set spell filetype=php: -->
 
-<p> Most of us have noticed that the sky darkens a short while before an 
-afternoon downpour.  And keen weather observers have the patience to monitor a 
-darkening over several hours before fronts arrive.  These are examples of 
-<i>local</i> weather prediction based on variations in sky brightness.   
-<i>Regional</i>  predictions could be made, if observations could be tied together.
-For example, a storm running up the coast of Nova Scotia might darken Lunenburg 
-skies a few hours before anything could be observed in Halifax.</p>
+<p>Most of us have noticed that the sky darkens in the minutes before an 
+afternoon downpour.  Patient weather observers witness hours of darkening, as a 
+front rolls in.  In effect, these are local, short-term weather predictions 
+based on sky brightness.   Regional, longer-term predictions could also be 
+made, if observations could be linked together.  For example, a storm running 
+up the coast of Nova Scotia would darken Lunenburg skies well in advance
+of bad weather in Halifax.</p>
 
 <p>The question is how to set up a network with enough stations to be of use.  
-The Skynet program proposes an answer: put sensors in schools.  This will give 
-excellent spatial coverage, because all populated areas have schools.  And, 
-since schools already have computers, data recording will not require new 
-infrastructure.  As for the sensors, students are capable of building them, 
-calibrating them, and interfacing them to computers.</p>
+This could be done by Environment Canada, at great expense.  But why should
+civil servants have all the fun?  With today's technology, the whole program 
+could be done in schools, by students.  That is the idea of SkyView.</p>
 
-<p>This seems to be an exercise in child labour.  Far from it.  Actually, 
-SkyNet is a thinly-disguised learning experience.  And it is a broad learning 
-experience, indeed. Science-minded students would benefit from observing the 
-atmosphere, and thinking about the general Physics involved, including 
-convection, waves, and fronts.  Students with interests in engineering and 
-computing would gain much from setting up and calibrating the equipment, and 
-linking sensors to computers.  There are obvious opportunities for students 
-interested in statistics.  Management-minded students could organize the work 
-teams, while business-minded students could take care of the finances and deal 
-with local sponsors.  Writers will be needed to explain the work, and public 
-speakers could communicate with the media and the public.  SkyNet will offer 
-insights on history, because observing the sky was once a serious thing, for 
-navigation, agriculture, and so forth.  The list goes on.  Simply stated, 
-SkyNet could offer much, to many students.</p>
+<p>Putting sensors in schools will give excellent spatial coverage, because all 
+populated areas have schools.  Since schools already have computers, data 
+logging and transmission will not require new infrastructure.  As for the work 
+of building sensors, calibrating them, connecting them to a network, creating 
+analysis tools, etc., this could all be done by the students.  This is the real 
+point of SkyView: to show students that they are capable of doing cutting-edge 
+scientific and technical work.</p> 
 
-<p>There project need not be limited to the measurement of light.  With modern
-micro-controllers and sensors, students could measure many aspects of weather
-that were once the domain of the professionals.  Students are both enthusiastic
-and capable, as the popularity of robotic competitions indicates.</p>
+<p>SkyView offers broad learning experiences, in environmental sciences, basic 
+sciences, mathematics, and computing.  There are also roles for students more 
+interested in business and management (organizing the scientists!), in communication 
+and public relations (websites, lectures, etc.), in
+such fields as archaeology (observatories like Stonehenge) and history 
+(navigation).</p>
 
-<p>It seems likely that people would start to consult SkyNet for informal
-short-term local weather forecasts.  It is not hard to imagine the pride that
-students would feel when that happens.</p>
+<p>There project need not be limited to the measurement of light.  With modern 
+micro-controllers and sensors, students could measure many aspects of weather 
+that were once the domain of the professionals.  The popularity of robot 
+competitions in schools is proof that students have the ability to do this sort 
+of work, and the interest to do so.</p>
 
+<p>Over time, members of the public would start to consult SkyNet for informal 
+short-term local weather forecasts.  The pride that students would feel, 
+knowing that, is justification enough for the project.</p>
