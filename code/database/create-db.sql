9e3e51df96ee4447e2a04198ec9fbe65ccada8a1
Dan Kelley <kelley.dan@gmail.com>
2011-04-11 18:33:37 -0300

    clean up

--- code/database/create-db.sql
+++ code/database/create-db.sql
@@ -1,15 +1,15 @@
 -- sqlite3 skyview.db < create-db.sql
 CREATE TABLE observations(
     id integer primary key,
-    station_id int, -- used with table 'sensors'
-    time datetime, -- 2011-04-11 09:51:38 ADT
+    station_id int,
+    time datetime,
     light_mean real,
     light_stddev real);
 CREATE TABLE stations(
-    id integer primary key, -- use for JOIN with table 'observations'
-    latitude real, -- use negative for southern hemisphere
-    longitude real, -- use negative for western hemisphere, e.g. Canada
-    direction_id int, --enu('north', 'northeast','east','southeast','south','southwest','west','northeast','vertical'),
+    id integer primary key,
+    latitude real, -- negative for southern hemisphere
+    longitude real, -- negative for western hemisphere, e.g. Canada
+    direction_id int,
     name varchar(100));
 CREATE TABLE directions(
     id integer primary key,
