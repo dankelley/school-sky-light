77b9dec2d5383d73aedef22a642908e2e73508f8
Dan Kelley <kelley.dan@gmail.com>
2011-04-11 17:26:00 -0300

    updatd database schema

---
+++ code/database/add-data.sql
@@ -0,0 +1,3 @@
+-- sqlite3 skyview.db < add-data.sql
+INSERT INTO observations (time,station_id,light_mean,light_stddev) VALUES("2011-04-11 15:07:00",1, 90, 1);
+
--- code/database/add-station.sql
+++ code/database/add-station.sql
@@ -1,3 +1,3 @@
 -- sqlite3 skyview.db < add-station.sql
-INSERT INTO `sensors` (latitude, longitude, direction, name) VALUES( 44.635530, -63.594621, "west", "Dalhousie University 1");
+INSERT INTO stations (latitude, longitude, direction_id, name) VALUES(44.635530, -63.594621, 1, "Dalhousie University 1");
 
