56c22fc05f27361ae94aa6e1fd4f504436d13f51
Dan Kelley <kelley.dan@gmail.com>
2011-04-11 17:26:00 -0300

    updatd database schema

--- code/database/create-db.sql
+++ code/database/create-db.sql
@@ -1,14 +1,26 @@
 -- sqlite3 skyview.db < create-db.sql
 CREATE TABLE observations(
     id integer primary key,
-    sensor int, -- used with table 'sensors'
-    time varchar(23), -- 2011-04-11 09:51:38 ADT
+    station_id int, -- used with table 'sensors'
+    time datetime, -- 2011-04-11 09:51:38 ADT
     light_mean real,
     light_stddev real);
-CREATE TABLE sensors(
+CREATE TABLE stations(
     id integer primary key, -- use for JOIN with table 'observations'
     latitude real, -- use negative for southern hemisphere
     longitude real, -- use negative for western hemisphere, e.g. Canada
-    direction varchar(20), -- 'east', 'west', 'north', 'south', 'northeast', etc
+    direction_id int, --enu('north', 'northeast','east','southeast','south','southwest','west','northeast','vertical'),
     name varchar(100));
+CREATE TABLE directions(
+    id integer primary key,
+    heading varchar(20));
+INSERT INTO directions (heading) VALUES ("north");
+INSERT INTO directions (heading) VALUES ("northeast");
+INSERT INTO directions (heading) VALUES ("east");
+INSERT INTO directions (heading) VALUES ("southeast");
+INSERT INTO directions (heading) VALUES ("south");
+INSERT INTO directions (heading) VALUES ("southwest");
+INSERT INTO directions (heading) VALUES ("west");
+INSERT INTO directions (heading) VALUES ("northwest");
+INSERT INTO directions (heading) VALUES ("omnidirectional");
 
