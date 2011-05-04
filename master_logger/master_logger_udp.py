d4e44d34c6d9627f0a9b8db13f3c68608761a6aa
Dan Kelley <kelley.dan@gmail.com>
2011-05-04 15:08:14 -0300

    master_logger_udp now secretized

--- master_logger/master_logger_udp.py
+++ master_logger/master_logger_udp.py
@@ -2,28 +2,27 @@ import socket
 import serial
 from time import sleep, time
 from string import atoi
-from sys import argv, exit
-UDP_IP="129.173.23.36" # emit.phys.ocean.dal.ca
-UDP_PORT=5005
-sensor_code=1
-# FIXME: udp work
+import sys
+sys.path.append("../")
+import secret
+
 # FIXME: database work
-if 5 != len(argv):
+if 5 != len(sys.argv):
     print "Usage examples:"
-    print "  python " + argv[0] +  " 10 60 /dev/tty.usbmodem411    /Users/kelley/Sites/skyview/skyview.db"
-    print "  python " + argv[0] +  " 10 60 /dev/tty.usbmodemfa2131 /Users/kelley/Sites/skyview/skyview.db"
+    print "  python " + sys.argv[0] +  " 10 60 /dev/tty.usbmodem411    /Users/kelley/Sites/skyview/skyview.db"
+    print "  python " + sys.argv[0] +  " 10 60 /dev/tty.usbmodemfa2131 /Users/kelley/Sites/skyview/skyview.db"
     exit(2)
-sampling_interval = atoi(argv[1])
-reporting_interval = atoi(argv[2])
-usb = argv[3]
+sampling_interval = atoi(sys.argv[1])
+reporting_interval = atoi(sys.argv[2])
+usb = sys.argv[3]
 def meanstd(x):
     from math import sqrt
     n, mean, std = len(x), 0, 0
     for a in x:
-	mean = mean + a
+        mean = mean + a
     mean = mean / float(n)
     for a in x:
-	std = std + (a - mean)**2
+        std = std + (a - mean)**2
     std = sqrt(std / float(n-1))
     return mean, std
 
@@ -51,8 +50,8 @@ while (True):
                     light_mean, light_stdev = meanstd(values)
                     time_mean, time_stdev = meanstd(times)
                     #print int(round(time_mean)), int(round(light_mean)), int(round(light_stdev))
-                    msg = "%d %d %d %d" % (sensor_code, int(round(time_mean)), int(round(light_mean)), int(round(light_stdev)))
-                    sock.sendto(msg, (UDP_IP, UDP_PORT))
+                    msg = "%d %4s %d %d %d" % (secret.station_id, secret.station_code, int(round(time_mean)), int(round(light_mean)), int(round(light_stdev)))
+                    sock.sendto(msg, (secret.aggregator_ip, secret.aggregator_port))
                     times = []
                     values = []
             ser.write('>')
