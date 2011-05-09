5e20d051c44e883795f0c083a2ba294603613035
Dan Kelley <kelley.dan@gmail.com>
2011-05-09 09:20:44 -0300

    light_clock: put midnight on bottom

--- Makefile
+++ Makefile
@@ -4,13 +4,11 @@ all: README.md
 
 force:
 webt: force
-	cp skyview-01/msl.c ~/Sites/skyviewt/code
 	cp web/*.php web/*.css ~/Sites/skyviewt
 	cp web/images/* ~/Sites/skyviewt/images
 	cp web/code/*.R ~/Sites/skyviewt/code
 
 web: force
-	cp skyview-01/msl.c ~/Sites/skyview/code
 	cp web/*.php web/*.css ~/Sites/skyview
 	cp web/images/* ~/Sites/skyview/images
 	cp web/code/*.R ~/Sites/skyview/code
--- web/code/light_clock.R
+++ web/code/light_clock.R
@@ -12,22 +12,24 @@ days <- round(as.numeric(difftime(rt[2],rt[1],"days")))
 hour <- t$hour + t$min / 60
 light.scale <- 1 * max(light)
 r <- 1 + light / light.scale
-x <- r * sin(2*pi*hour/24)
-y <- r * cos(2*pi*hour/24)
+x <- r * sin(2*pi*(hour-12)/24)
+y <- r * cos(2*pi*(hour-12)/24)
 png("light_clock.png", width=500, height=500, pointsize=14)
 par(mar=c(1,1,1,1))
-drawpalette(c(1,days), zlab="Day of sampling", col=oceColorsJet, top=1.5, bottom=1.5)
+drawPalette(c(1,days), zlab="Day of sampling", col=oceColorsJet, top=0.3, bottom=0.3)
 col <- oceColorsJet(100)[rescale(as.numeric(time), rlow=1, rhigh=100)]
 plot(x,y,asp=1,cex=1/3,axes=FALSE, xlab="", ylab="", col=col)
-xc <- sin(2*pi*seq(0,23,0.1)/24)
-yc <- cos(2*pi*seq(0,23,0.1)/24)
+h <- seq(0, 23, 0.1) - 12
+xc <- sin(2*pi*h/24)
+yc <- cos(2*pi*h/24)
 lines(xc, yc)
-xc <- sin(2*pi*seq(0,23,1)/24)
-yc <- cos(2*pi*seq(0,23,1)/24)
-xcc <- .9*sin(2*pi*seq(0,23,1)/24)
-ycc <- .9*cos(2*pi*seq(0,23,1)/24)
+h <- seq(0, 23, 1) - 12
+xc <- sin(2*pi*(h-12)/24)
+yc <- cos(2*pi*(h-12)/24)
+xcc <- .9*xc
+ycc <- .9*yc
 segments(xc, yc, xcc, ycc)
-xcc <- .8*sin(2*pi*seq(0,23,1)/24)
-ycc <- .8*cos(2*pi*seq(0,23,1)/24)
-text(xcc, ycc, 0:23)
+xcc <- .8*sin(2*pi*h/24)
+ycc <- .8*cos(2*pi*h/24)
+text(xcc, ycc, seq(0,23,1))
 text(0, 0, "site: skyview-01\ncolor: time in series\nhour: UTC")
