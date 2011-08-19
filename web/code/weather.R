a419205f4a74f2710fff40c28fca199f5a2e72e3
Dan Kelley <kelley.dan@gmail.com>
2011-08-19 08:27:25 -0300

    weather just for last week

--- web/code/weather.R
+++ web/code/weather.R
@@ -5,7 +5,10 @@ con <- dbConnect(m, dbname="../skyview.db")
 observations <- dbGetQuery(con, "select time,light_mean from observations")
 t <- numberAsPOSIXct(observations$time) # timezone?
 light <- 100 * ((1023 - observations$light_mean) / 1023)
+now <- as.POSIXct(Sys.time())
+look <- (now - 7*86400) < t
 png("weather.png", width=900, height=200, pointsize=13)
-oce.plot.ts(t, light, type='l', ylab='Light Intensity (per cent)', ylim=c(0, 100))
+oce.plot.ts(t[look], light[look], type='l', ylab='Light Intensity (per cent)', ylim=c(0, 100))
 load("~/pressure-halifax.rda")
 lines(station8539$time, 10 + 5*(station8539$pressure - mean(station8539$pressure,na.rm=TRUE)), col='lightblue', lwd=3)
+dev.off()
