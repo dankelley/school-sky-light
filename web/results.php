ad7115aab37380ab6ce281581ed8f553174f2c84
Dan Kelley <kelley.dan@gmail.com>
2011-09-01 08:47:20 -0300

    typo

--- web/results.php
+++ web/results.php
@@ -23,7 +23,7 @@ if ("$subtab" == "weather") {
         <p>The graph shown below indicates recent variations in light 
         intensity in an office in Halifax, Nova Scotia.  The graph is made by 
         an R script called <a href="code/weather.R">weather.R</a>, which is run 
-        every 10 minutes, using data mesured every 10 seconds and formed into 
+        every 10 minutes, using data measured every 10 seconds and formed into 
         1-minute averages.  The sensor is placed on a desk, pointing upwards, 
         and shaded from direct sunlight.  The (rare) use of lights in the 
         office is indicated by near-constant light levels of 80 percent.  The 
