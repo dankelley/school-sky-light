7d1964a5aec82afca15a29b2bdbc07a060ea004d
dankelley <kelley.dan@gmail.com>
2018-12-20 10:37:30 -0400

    solar_navigation.R

--- web/code/solar_navigation.R
+++ web/code/solar_navigation.R
@@ -13,14 +13,15 @@ hfx.sun.angle <- function(t) sunAngle(t, lat=44+39/60, lon=-(63+36/60), useRefra
 
 library(RSQLite)
 m <- dbDriver("SQLite")
-con <- dbConnect(m, dbname="../skyview.db")
+con <- dbConnect(m, dbname="~/Dropbox/skyview.db")
 observations <- dbGetQuery(con, "select time,light_mean from observations")
+dbDisconnect(con)
 time <- numberAsPOSIXct(observations$time) # timezone?
 light <- 100 * ((1023 - observations$light_mean) / 1023)
 look <- time > (max(time) - windowDays*86400)
 time <- time[look]
 light <- light[look]
-light.smoothed <- smooth(light)        
+light.smoothed <- smooth(light)
 ## kernapply(as.numeric(runmed(light, k=3)), kernel("daniell", 2), circular=TRUE)
 oce.plot.ts(time, light.smoothed, type='l', ylim=c(0, 100), ylab="Smoothed light intensity (percent)")
 dim.light <- ifelse(light.smoothed < 5, light, runif(length(light), 0, 5))
@@ -48,7 +49,7 @@ nrises <- length(rises)
 ## Indicate on the graph
 rug(rises, col='red', lwd=4)
 rug(sets, col='blue', lwd=4)
-legend("topleft", lwd=c(1, 1, 1), 
+legend("topleft", lwd=c(1, 1, 1),
        col=c("black", "red", "blue"),
        legend=c("Observed", "Sunrise", "Sunset"),
        cex=3/4, bg="white")
@@ -61,22 +62,23 @@ if (!interactive())
 latHfx <- 44.65
 lonHfx <- (-63.55274)
 par(mfrow=c(1,1))
-data(coastlineWorld)
-plot(coastlineWorld, clatitude=latHfx, clongitude=lonHfx, span=5000)
+data(coastlineWorldMedium, package="ocedata")
+plot(coastlineWorldMedium, projection="+proj=lcc +lon+0=-63 +lat_0=45 +lat_1=35 +lat_2=55",
+     clatitude=latHfx, clongitude=lonHfx, span=3000, col="gray")
 ## Find lat and lon using all sunrises and sunsets
 o <- optim(c(1,1), mismatch, hessian=TRUE)
-lat <- o$par[1]
-lon <- o$par[2]
+lon <- o$par[1]
+lat <- o$par[2]
 ## Indicate the spot on a map, and show the uncertainty
 latErr <- sqrt(abs(o$value / o$hessian[1,1])) / 2
 lonErr <- sqrt(abs(o$value / o$hessian[2,2])) / 2
-lines(rep(lon, 2), lat + latErr * c(-1, 1), lwd=3, col='red')
-lines(lon + lonErr*c(-1, 1), rep(lat, 2), lwd=3, col='red')
-points(lonHfx, latHfx, pch=0, cex=2, col='blue', lwd=2)
-points(lon, lat, pch=1, cex=2, col='red', lwd=2)
+mapLines(rep(lon, 2), lat + latErr * c(-1, 1), lwd=3, col='red')
+mapLines(lon + lonErr*c(-1, 1), rep(lat, 2), lwd=3, col='red')
+mapPoints(lonHfx, latHfx, pch=0, cex=2, col='blue', lwd=2)
+mapPoints(lon, lat, pch=1, cex=2, col='red', lwd=2)
 cat("lon=", lon, "lat=", lat, "distance", geodDist(lonHfx, latHfx, lon, lat), "\n")
-legend("topright", col=c("blue", "red"), pch=c(0,1), pt.cex=2,
-       legend=c("Actual", "Inferred"))
+legend("bottomright", col=c("blue", "red"), pch=c(0,1), pt.cex=2,
+       pt.lwd=3, legend=c("Actual", "Inferred"), bg="white")
 if (!interactive())
     dev.off()
 
