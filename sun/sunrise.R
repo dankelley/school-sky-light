27ac35c9b5981ae2ce96c5b05fa1cfb08804c43c
dankelley <kelley.dan@gmail.com>
2021-06-01 09:34:10 -0300

    sunrise.R

--- sun/sunrise.R
+++ sun/sunrise.R
@@ -1,8 +1,7 @@
-##http://www.timeanddate.com/worldclock/astronomy.html?n=286&month=2&year=2011&obj=sun&afl=-11&day=1
 library(oce)
 use.refraction <- TRUE
-elevation <- function(x) { # given longitude
-    mean(sunAngle(t0, x[1], lon[i], use.refraction)$elevation^2)^0.5
+elevation <- function(x) { # x is latitude
+    mean(sunAngle(t0, lon[i], x[1], use.refraction)$altitude^2)^0.5
 }
 hlat <-   44+38/60                     # http://www.timeanddate.com/worldclock/city.html?n=286
 hlon <- -(63+35/60)                    # http://www.timeanddate.com/worldclock/city.html?n=286
@@ -10,11 +9,11 @@ d <- read.table('sunrise.dat', header=FALSE)
 tz <- 4 * 3600
 sunrise <- as.POSIXct(paste(d$V1, d$V2), tz="UTC") + tz
 sunset <- as.POSIXct(paste(d$V1, d$V3), tz="UTC") + tz + 12 * 3600
-print(data.frame(sunrise,sunset))
 
-data(coastline.world)
+data(coastlineWorldFine, package="ocedata")
 
-pdf("map-with-lines.pdf", width=8, height=10.5, pointsize=12)
+if (!interactive())
+    png("map-with-lines.png", unit="in", width=8, height=10.5, res=200, pointsize=12)
 n <- 100
 lon <- seq(-180, 180, length.out=n)
 lat <- vector(length=n)
@@ -22,8 +21,8 @@ t0 <- sunrise[1]
 for (i in 1:n) {
     lat[i] <- optimise(elevation, interval=c(-90,90))$minimum
 }
-plot(coastline.world, center=c(hlat, hlon), span=1500)
-abline(h=hlat, v=hlon, col='gray')
+plot(coastlineWorldFine, clon=hlon, clat=hlat, span=1500)
+points(hlon, hlat, cex=3, lwd=3, col=2)
 lines(lon, lat, lwd=2)
 t0 <- sunset[1]
 for (i in 1:n) {
@@ -33,5 +32,7 @@ for (i in 1:n) {
 lines(lon, lat, lwd=2, lty="dashed")
 legend("bottomright", lwd=c(2,2), lty=c("solid", "dashed"),legend=c("Sunrise", "Sunset"))
 title(format(t0, "%Y-%b-%d"))
-dev.off()
+
+if (!interactive())
+    dev.off()
 
